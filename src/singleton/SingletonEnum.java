86f1e31b01be9d81fdc3b39f03fdf083f1a1c3e7
neeld <neeldc2@gmail.com>
2024-07-01 03:52:33 +0530

    Design patterns - Singleton

---
+++ src/singleton/SingletonEnum.java
@@ -0,0 +1,15 @@
+package singleton;
+
+public enum SingletonEnum {
+    INSTANCE;
+
+    int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
---
+++ src/singleton/SingletonEnumMain.java
@@ -0,0 +1,11 @@
+package singleton;
+
+public class SingletonEnumMain {
+    public static void main(String[] args) {
+        SingletonEnum singleton = SingletonEnum.INSTANCE;
+
+        System.out.println(singleton.getValue());
+        singleton.setValue(2);
+        System.out.println(singleton.getValue());
+    }
+}
